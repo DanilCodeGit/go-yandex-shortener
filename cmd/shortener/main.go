9f779c8059cbaa6533b172daab92455c7914c23c
Danil <naumkin-02@mail.ru>
2023-09-05 19:07:31 +0300

    3rd commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,67 +1,76 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 var urlStore = make(map[string]string)
 
-func ShortenURL(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "It's not a post-request", http.StatusMethodNotAllowed)
-		return
+func HandleURL(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+
+		// Разбить путь запроса на части
+		parts := strings.Split(r.URL.Path, "/")
+
+		// Извлечь значение {id}
+		if len(parts) < 2 || parts[1] == "" {
+			http.Error(w, "Некорректный запрос", http.StatusBadRequest)
+			return
+		}
+		id := parts[1]
+		// Ваша логика для получения оригинального URL на основе id.
+		originalURL := urlStore[id]
+		//fmt.Fprintf(w, "%s", originalURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Header().Set("Location", originalURL)
+
+	case http.MethodPost:
+		// Read the URL from the request body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		// Convert the request body to a string
+		url := string(body)
+
+		shortURL := hashURL(url) //generateShortURL()
+		urlStore[shortURL] = url
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+
+		fmt.Fprintf(w, "http://localhost:8080/%s\n", shortURL)
+	default:
+		http.Error(w, "Метод не разрешен", http.StatusMethodNotAllowed)
 	}
-
-	// Read the URL from the request body
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Convert the request body to a string
-	url := string(body)
-
-	shortURL := "EwHXdJfB" //generateShortURL()
-	urlStore[shortURL] = url
-
-	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "http://localhost:8080/%s", shortURL)
 }
 
-func RedirectURL(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Метод не разрешен", http.StatusMethodNotAllowed)
-		return
-	}
+func hashURL(urlToHash string) string {
+	// Создайте новый хеш SHA-256
+	hasher := sha256.New()
 
-	// Получаем значение параметра {id} из URL
-	id := r.URL.Path[1:] // Убираем первый символ ("/")
+	// Преобразуйте URL в байтовый срез и передайте его хешеру
+	hasher.Write([]byte(urlToHash))
 
-	// Здесь вы можете добавить логику для сопоставления id с оригинальным URL.
-	// Например, вы можете использовать карту (map) для хранения соответствий.
+	// Получите байтовое представление хеша
+	hashBytes := hasher.Sum(nil)
 
-	// Если id не найден, вернем код 400
-	if id == "" {
-		http.Error(w, "Некорректный запрос", http.StatusBadRequest)
-		return
-	}
+	// Преобразуйте байты хеша в строку в шестнадцатеричном формате
+	hashedURL := hex.EncodeToString(hashBytes)
 
-	// Получите оригинальный URL на основе id (замените на вашу логику).
-
-	// В случае успешной обработки запроса, вернем код 307 и оригинальный URL
-	originalURL := "https://practicum.yandex.ru/" // Замените на ваш оригинальный URL
-	w.Header().Set("Location", originalURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
+	return hashedURL
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, ShortenURL)
-	mux.HandleFunc(`/EwHXdJfB`, RedirectURL)
+	mux.HandleFunc(`/`, HandleURL)
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
