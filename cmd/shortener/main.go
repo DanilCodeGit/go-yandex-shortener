ef277b491aa60f6672e49d9ec9348cab2cdb7577
Danil <naumkin-02@mail.ru>
2023-09-10 17:50:07 +0300

    1st commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,59 +11,6 @@ import (
 
 var urlStore = make(map[string]string)
 
-func handleROOT(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		handleGet(w, r)
-	}
-	if r.Method == http.MethodPost {
-		handlePost(w, r)
-	}
-}
-
-func handleGet(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		// Разбить путь запроса на части
-		parts := strings.Split(r.URL.Path, "/")
-
-		// Извлечь значение {id}
-		if len(parts) < 2 || parts[1] == "" {
-			http.Error(w, "Некорректный запрос", http.StatusBadRequest)
-			return
-		}
-		id := parts[1]
-		// Ваша логика для получения оригинального URL на основе id.
-		originalURL := urlStore[id]
-		w.Header().Set("Location", originalURL)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	} else {
-		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-}
-func handlePost(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		// Read the URL from the request body
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		// Convert the request body to a string
-		url := string(body)
-
-		shortURL := hashURL(url) //generateShortURL()
-		urlStore[shortURL] = url
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusCreated)
-
-		fmt.Fprintf(w, "http://localhost:8080/%s", shortURL)
-	} else {
-		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-}
-
 func hashURL(urlToHash string) string {
 	// Создайте новый хеш SHA-256
 	hasher := sha256.New()
@@ -78,6 +25,52 @@ func hashURL(urlToHash string) string {
 	hashedURL := hex.EncodeToString(hashBytes)
 
 	return hashedURL
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	// Разбить путь запроса на части
+	parts := strings.Split(r.URL.Path, "/")
+
+	// Извлечь значение {id}
+	if len(parts) < 2 || parts[1] == "" {
+		http.Error(w, "Некорректный запрос", http.StatusBadRequest)
+		return
+	}
+	id := parts[1]
+	// Ваша логика для получения оригинального URL на основе id.
+	originalURL := urlStore[id]
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	// Read the URL from the request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	if len(body) == 0 {
+		http.Error(w, "Тело запроса пустое", http.StatusBadRequest)
+		return
+	}
+	// Convert the request body to a string
+	url := string(body)
+
+	ShortURL := hashURL(url) //generateShortURL()
+	urlStore[ShortURL] = url
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+
+	fmt.Fprintf(w, "http://localhost:8080/%s", ShortURL)
+}
+
+func handleROOT(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		handleGet(w, r)
+	}
+	if r.Method == http.MethodPost {
+		handlePost(w, r)
+	}
 }
 
 func main() {
---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_handlePost(t *testing.T) {
+	urlStore = map[string]string{}
+	type want struct {
+		statusCode  int
+		responseURL string
+	}
+	tests := []struct {
+		name        string
+		requestBody string
+		want        want
+	}{
+		{
+			name:        "Empty URL",
+			requestBody: "",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				responseURL: "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body := strings.NewReader(tt.requestBody)
+			req, err := http.NewRequest("POST", "/", body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rr := httptest.NewRecorder()
+			handlePost(rr, req)
+
+			if rr.Code != tt.want.statusCode {
+				t.Errorf("got status code %d, want %d", rr.Code, tt.want.statusCode)
+			}
+
+			if rr.Code == http.StatusCreated {
+				responseBody := rr.Body.String()
+				if responseBody != tt.want.responseURL {
+					t.Errorf("got response body %s, want %s", responseBody, tt.want.responseURL)
+				}
+			}
+		})
+	}
+}
+
+func Test_handleGet(t *testing.T) {
+	urlStore = map[string]string{
+		"abc123": "http://example.com",
+		"def456": "http://example.org",
+	}
+	type want struct {
+		statusCode  int
+		originalURL string
+	}
+	tests := []struct {
+		name       string
+		requestURL string
+		want       want
+	}{
+		{
+			name:       "Valid URL",
+			requestURL: "/abc123",
+			want: want{
+				statusCode:  http.StatusTemporaryRedirect,
+				originalURL: "http://example.com",
+			},
+		},
+		{
+			name:       "Nonexistent URL",
+			requestURL: "/nonexistent",
+			want: want{
+				statusCode:  http.StatusTemporaryRedirect,
+				originalURL: "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", tt.requestURL, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			w := httptest.NewRecorder()
+			handleGet(w, req)
+			res := w.Result()
+			assert.Equal(t, res.StatusCode, tt.want.statusCode)
+
+			if res.StatusCode == http.StatusTemporaryRedirect {
+				locationHeader := w.Header().Get("Location")
+				assert.Equal(t, locationHeader, tt.want.originalURL)
+
+			}
+		})
+	}
+}
