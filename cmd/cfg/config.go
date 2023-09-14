bf2260e384adc3c95787c6f97694ad62df2a46bf
Danil <naumkin-02@mail.ru>
2023-09-14 15:12:48 +0300

    iter5 1

--- cmd/cfg/config.go
+++ cmd/cfg/config.go
@@ -1,20 +1,28 @@
 package cfg
 
-//import (
-//	"flag"
-//	"fmt"
-//)
-//
-//var (
-//	listenAddr = flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера")
-//	baseURL    = flag.String("b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
-//)
-//
-//func MyFlags() {
-//
-//	flag.Parse()
-//	// Вывод значений флагов
-//	fmt.Println("Адрес сервера:", listenAddr)
-//	fmt.Println("Базовый адрес URL:", baseURL)
-//
-//}
+import (
+	"flag"
+	"log"
+
+	"github.com/caarlos0/env"
+)
+
+// Переменные окружения
+type Config struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
+}
+
+func Env() {
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Флаги
+var (
+	FlagServerAddress = flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера")
+	FlagBaseURL       = flag.String("b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
+)
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,19 +9,10 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/DanilCodeGit/go-yandex-shortener/cmd/cfg"
 	"github.com/go-chi/chi/v5"
 )
 
-// var (
-//
-//	listenAddr = flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера")
-//	baseURL    = flag.String("b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
-//
-// )
-var (
-	listenAddr = flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера")
-	baseURL    = flag.String("b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
-)
 var urlStore = make(map[string]string)
 
 func hashURL(urlToHash string) string {
@@ -74,17 +65,102 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 	urlStore[ShortURL] = url
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "%s/%s", *baseURL, ShortURL)
-	//fmt.Fprintf(w, "http://localhost:8080/%s", ShortURL)
+	fmt.Fprintf(w, "%s/%s", *cfg.FlagBaseURL, ShortURL)
 }
 
 func main() {
+	cfg.Env()
 	flag.Parse()
 	r := chi.NewRouter()
 	r.Get("/{id}", handleGet)
 	r.Post("/", handlePost)
-	err := http.ListenAndServe(*listenAddr, r)
+	err := http.ListenAndServe(*cfg.FlagServerAddress, r)
 	if err != nil {
 		panic(err)
 	}
 }
+
+//package main
+//
+//import (
+//	"crypto/sha256"
+//	"encoding/hex"
+//	"flag"
+//	"fmt"
+//	"io"
+//	"net/http"
+//	"strings"
+//
+//	"github.com/go-chi/chi/v5"
+//)
+//
+//var (
+//	listenAddr = flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера")
+//	baseURL    = flag.String("b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
+//)
+//var urlStore = make(map[string]string)
+//
+//func hashURL(urlToHash string) string {
+//	// Создайте новый хеш SHA-256
+//	hasher := sha256.New()
+//
+//	// Преобразуйте URL в байтовый срез и передайте его хешеру
+//	hasher.Write([]byte(urlToHash))
+//
+//	// Получите байтовое представление хеша
+//	hashBytes := hasher.Sum(nil)
+//
+//	// Преобразуйте байты хеша в строку в шестнадцатеричном формате
+//	hashedURL := hex.EncodeToString(hashBytes)
+//
+//	return hashedURL
+//}
+//
+//func handleGet(w http.ResponseWriter, r *http.Request) {
+//	// Разбить путь запроса на части
+//	parts := strings.Split(r.URL.Path, "/")
+//
+//	// Извлечь значение {id}
+//	if len(parts) < 2 || parts[1] == "" {
+//		http.Error(w, "Некорректный запрос", http.StatusBadRequest)
+//		return
+//	}
+//	id := parts[1]
+//	// Ваша логика для получения оригинального URL на основе id.
+//	originalURL := urlStore[id]
+//	w.Header().Set("Location", originalURL)
+//	w.WriteHeader(http.StatusTemporaryRedirect)
+//}
+//
+//func handlePost(w http.ResponseWriter, r *http.Request) {
+//	// Read the URL from the request body
+//	body, err := io.ReadAll(r.Body)
+//	if err != nil {
+//		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+//		return
+//	}
+//	if len(body) == 0 {
+//		http.Error(w, "Тело запроса пустое", http.StatusBadRequest)
+//		return
+//	}
+//	// Convert the request body to a string
+//	url := string(body)
+//
+//	ShortURL := hashURL(url) //generateShortURL()
+//	urlStore[ShortURL] = url
+//	w.Header().Set("Content-Type", "text/plain")
+//	w.WriteHeader(http.StatusCreated)
+//	fmt.Fprintf(w, "%s/%s", *baseURL, ShortURL)
+//	//fmt.Fprintf(w, "http://localhost:8080/%s", ShortURL)
+//}
+//
+//func main() {
+//	flag.Parse()
+//	r := chi.NewRouter()
+//	r.Get("/{id}", handleGet)
+//	r.Post("/", handlePost)
+//	err := http.ListenAndServe(*listenAddr, r)
+//	if err != nil {
+//		panic(err)
+//	}
+//}
