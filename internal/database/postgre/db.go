11f461faad9397b1bcc97047d3f9ea479d32479a
Danil <naumkin-02@mail.ru>
2023-10-09 22:20:11 +0300

    #7 iter10

--- internal/database/postgre/db.go
+++ internal/database/postgre/db.go
@@ -28,7 +28,8 @@ func DBConn() error {
 	conn, err := pgx.Connect(context.Background(), *cfg.FlagDataBaseDSN)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		//os.Exit(1)
+		return err
 	}
 	defer conn.Close(context.Background())
 	log.Println("Успешное подключение")
