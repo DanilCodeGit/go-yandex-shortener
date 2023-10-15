c8bed817d40c2c5930ee62418399901edca463ea
Danil <naumkin-02@mail.ru>
2023-10-15 12:39:31 +0300

    #22 iter11

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,14 +12,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	//"strings"
+	"strings"
 	"sync"
 
 	"github.com/DanilCodeGit/go-yandex-shortener/internal/cfg"
 	"github.com/DanilCodeGit/go-yandex-shortener/internal/database/postgre"
 	"github.com/DanilCodeGit/go-yandex-shortener/internal/storage"
 	"github.com/DanilCodeGit/go-yandex-shortener/internal/tools"
-	"github.com/go-chi/chi/v5"
 )
 
 var st = storage.URLStore
@@ -65,8 +64,9 @@ func HandlePost(w http.ResponseWriter, r *http.Request) {
 	url := string(body)
 
 	shortURL := tools.HashURL(url)
-
+	mu.Lock()
 	st[shortURL] = url
+	mu.Unlock()
 
 	// Преобразование данных в формат JSON
 	jsonData := make(map[string]string)
@@ -80,7 +80,7 @@ func HandlePost(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Неудачное подключение")
 	}
-
+	defer conn.Close()
 	err = postgre.CreateTable(conn)
 	if err != nil {
 		log.Println("База не создана")
@@ -120,30 +120,27 @@ func JSONHandler(w http.ResponseWriter, req *http.Request) { //POST
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	var url string
-	for k, fullURL := range st {
-		delete(st, k)
-		url = fullURL
-	}
-
-	shortURL := tools.HashURL(url)
-	st[shortURL] = url
 
 	newData := make(map[string]string)
-	mu.Lock()
-	var full string
-	for shortURL, originalURL := range st {
-		newData[shortURL] = originalURL
-		full = originalURL
+	for _, v := range st {
+		newData[v] = v
 	}
-
+	hashedData := make(map[string]string)
+	for k, v := range newData {
+		hashedKey := tools.HashURL(k)
+		hashedData[hashedKey] = v
+	}
+	var shortURL, originalURL string
+	for i, v := range hashedData {
+		shortURL = i
+		originalURL = v
+	}
 	shortURL = "http://localhost:8080" + "/" + shortURL
 	responseData := map[string]string{"result": shortURL}
 	responseJSON, _ := json.Marshal(responseData)
 
-	mu.Unlock()
 	// Сохранение данных в файл после обновления
-	err = saveDataToFile(newData, *cfg.FlagFileStoragePath)
+	err = saveDataToFile(hashedData, *cfg.FlagFileStoragePath)
 	if err != nil {
 		http.Error(w, "Failed to save data to file", http.StatusInternalServerError)
 		return
@@ -159,7 +156,7 @@ func JSONHandler(w http.ResponseWriter, req *http.Request) { //POST
 		log.Println("База не создана")
 	}
 	// Сохраняем запрос в бд
-	err = postgre.SaveShortenedURL(conn, full, shortURL)
+	err = postgre.SaveShortenedURL(conn, originalURL, shortURL)
 	if err != nil {
 		log.Println("Запись не произошла")
 	}
@@ -168,26 +165,88 @@ func JSONHandler(w http.ResponseWriter, req *http.Request) { //POST
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "%v", string(responseJSON))
 
+	//var buf bytes.Buffer
+	//// читаем тело запроса
+	//_, err := buf.ReadFrom(req.Body)
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusBadRequest)
+	//	return
+	//}
+	//// Десереализуем json
+	//if err = json.Unmarshal(buf.Bytes(), &st); err != nil {
+	//	http.Error(w, err.Error(), http.StatusBadRequest)
+	//	return
+	//}
+	//var url string
+	//for k, fullURL := range st {
+	//	delete(st, k)
+	//	url = fullURL
+	//}
+	//
+	//shortURL := tools.HashURL(url)
+	//st[shortURL] = url
+	//
+	//newData := make(map[string]string)
+	//mu.Lock()
+	//var full string
+	//for shortURL, originalURL := range st {
+	//	newData[shortURL] = originalURL
+	//	full = originalURL
+	//}
+	//
+	//shortURL = "http://localhost:8080" + "/" + shortURL
+	//responseData := map[string]string{"result": shortURL}
+	//responseJSON, _ := json.Marshal(responseData)
+	//
+	//mu.Unlock()
+	//// Сохранение данных в файл после обновления
+	//err = saveDataToFile(newData, *cfg.FlagFileStoragePath)
+	//if err != nil {
+	//	http.Error(w, "Failed to save data to file", http.StatusInternalServerError)
+	//	return
+	//}
+	//////
+	//conn, err := postgre.DBConn()
+	//if err != nil {
+	//	log.Println("Неудачное подключение")
+	//}
+	//
+	//err = postgre.CreateTable(conn)
+	//if err != nil {
+	//	log.Println("База не создана")
+	//}
+	//// Сохраняем запрос в бд
+	//err = postgre.SaveShortenedURL(conn, full, shortURL)
+	//if err != nil {
+	//	log.Println("Запись не произошла")
+	//}
+	//
+	//w.Header().Set("Content-Type", "application/json")
+	//w.WriteHeader(http.StatusCreated)
+	//fmt.Fprintf(w, "%v", string(responseJSON))
+
 }
 
 func HandleGet(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
+	// Разбить путь запроса на части
+	parts := strings.Split(r.URL.Path, "/")
 
-	//// Разбить путь запроса на части
-	//parts := strings.Split(r.URL.Path, "/")
-	//
-	//// Извлечь значение {id}
-	//if len(parts) < 2 || parts[1] == "" {
-	//	http.Error(w, "Некорректный запрос", http.StatusBadRequest)
-	//	return
-	//}
-	//id := parts[1]
+	// Извлечь значение {id}
+	if len(parts) < 2 || parts[1] == "" {
+		http.Error(w, "Некорректный запрос", http.StatusBadRequest)
+		return
+	}
+	id := parts[1]
 	mu.Lock()
-	originalURL := storage.URLStore[id]
+	originalURL, exists := storage.URLStore[id]
+	if !exists {
+		log.Printf("OriginalUrl не найден: %v", http.StatusNotFound)
+	}
 	mu.Unlock()
 
 	w.Header().Set("Location", originalURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
+
 }
 
 func HandlePing(w http.ResponseWriter, r *http.Request) {
