438d3937817bf9d8820939bc1b8eaca682719881
Danil <naumkin-02@mail.ru>
2023-10-19 17:47:02 +0300

    iter13 #22

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"time"
 
@@ -33,6 +35,20 @@ func WithLogging(h http.Handler) http.Handler {
 			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
 			responseData:   responseData,
 		}
+
+		// Создаем буфер для записи тела запроса
+		var requestBodyBuffer bytes.Buffer
+		if r.Method == http.MethodPost || r.Method == http.MethodPut {
+			// Если метод POST или PUT, считываем тело запроса и записываем его в буфер
+			requestBody, err := io.ReadAll(r.Body)
+			if err != nil {
+				// Обработайте ошибку, если не удается считать тело запроса
+			} else {
+				requestBodyBuffer.Write(requestBody)
+				r.Body = io.NopCloser(bytes.NewBuffer(requestBody)) // Восстановите оригинальное тело запроса
+			}
+		}
+
 		h.ServeHTTP(&lw, r) // внедряем реализацию http.ResponseWriter
 
 		// Since возвращает разницу во времени между start
@@ -48,13 +64,73 @@ func WithLogging(h http.Handler) http.Handler {
 			"status", responseData.status, // получаем перехваченный код статуса ответа
 			"duration", duration,
 			"size", responseData.size,
-			"full url", responseData, // получаем перехваченный размер ответа
+			"request_body", requestBodyBuffer.String(), // получаем перехваченный размер ответа
 		)
 
 	}
 	// возвращаем функционально расширенный хендлер
 	return http.HandlerFunc(logFn)
 }
+
+//func WithLogging(h http.Handler) http.Handler {
+//	// создаём предустановленный регистратор zap
+//	logger, err := zap.NewDevelopment()
+//	if err != nil {
+//		// вызываем панику, если ошибка
+//		panic(err)
+//	}
+//	defer logger.Sync()
+//
+//	// делаем регистратор SugaredLogger
+//	sugar := logger.Sugar()
+//
+//	logFn := func(w http.ResponseWriter, r *http.Request) {
+//		start := time.Now()
+//
+//		responseData := &responseData{
+//			status: 0,
+//			size:   0,
+//		}
+//		lw := loggingResponseWriter{
+//			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+//			responseData:   responseData,
+//		}
+//
+//		// Создаем буфер для записи тела запроса
+//		var requestBodyBuffer bytes.Buffer
+//		if r.Method == http.MethodPost || r.Method == http.MethodPut {
+//			// Если метод POST или PUT, считываем тело запроса и записываем его в буфер
+//			requestBody, err := io.ReadAll(r.Body)
+//			if err != nil {
+//				// Обработайте ошибку, если не удается считать тело запроса
+//			} else {
+//				requestBodyBuffer.Write(requestBody)
+//				r.Body = io.NopCloser(bytes.NewBuffer(requestBody)) // Восстановите оригинальное тело запроса
+//			}
+//		}
+//
+//		h.ServeHTTP(&lw, r) // внедряем реализацию http.ResponseWriter
+//
+//		// Since возвращает разницу во времени между start
+//		// и моментом вызова Since. Таким образом можно посчитать
+//		// время выполнения запроса.
+//		duration := time.Since(start)
+//
+//		// отправляем сведения о запросе в zap
+//		sugar.Infow(
+//			"flags", *cfg.FlagFileStoragePath,
+//			"uri", r.RequestURI,
+//			"method", r.Method,
+//			"status", responseData.status, // получаем перехваченный код статуса ответа
+//			"duration", duration,
+//			"size", responseData.size,
+//			"request_body", requestBodyBuffer.String(), // выводим тело запроса
+//		)
+//	}
+//
+//	// возвращаем функционально расширенный хендлер
+//	return http.HandlerFunc(logFn)
+//}
 
 //
 
