f501e9049d64ea5b5c5040063ab3199c273a5458
Danil <naumkin-02@mail.ru>
2023-10-19 21:15:17 +0300

    iter13 #32

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,3 +1,33 @@
 package storage
 
+import "sync"
+
 var URLStore = make(map[string]string)
+
+type Storage struct {
+	URLsStore map[string]string
+	mu        sync.RWMutex
+}
+
+func NewStorage() *Storage {
+	return &Storage{
+		URLsStore: make(map[string]string),
+	}
+}
+func (s *Storage) SetURL(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.URLsStore[key] = value
+}
+func (s *Storage) GetURL(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	value := s.URLsStore[key]
+	return value, true
+}
+
+func (s *Storage) DeleteURL(key string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.URLsStore, key)
+}
