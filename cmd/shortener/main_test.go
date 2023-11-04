9d168a44cd2442d4d7c7c6ad30dd412c0e7bb767
Danil <naumkin-02@mail.ru>
2023-11-04 21:14:26 +0300

    iter15

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/DanilCodeGit/go-yandex-shortener/internal/cfg"
+	"github.com/DanilCodeGit/go-yandex-shortener/internal/database/postgre"
 	"github.com/DanilCodeGit/go-yandex-shortener/internal/handlers"
 	"github.com/magiconair/properties/assert"
 )
 
 func TestHandleGet(t *testing.T) {
 	// Создаем запрос с необходимым путем.
+	conn, _ := postgre.NewDataBase(context.Background(), *cfg.FlagDataBaseDSN)
 	req, err := http.NewRequest("GET", "/short-url", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -19,64 +23,8 @@ func TestHandleGet(t *testing.T) {
 	// Создаем записыватель ответа для захвата ответа.
 	rr := httptest.NewRecorder()
 	// Вызываем хэндлер.
-	handlers.HandleGet().ServeHTTP(rr, req)
+	handlers.HandleGet(conn).ServeHTTP(rr, req)
 	assert.Equal(t, rr.Code, http.StatusTemporaryRedirect)
 
 	// Добавляем другие проверки по мере необходимости.
 }
-
-//func TestHandlePost(t *testing.T) {
-//	// Создаем запрос с телом POST.
-//	conn, err := postgre.NewDataBase(context.Background(), *cfg.FlagDataBaseDSN)
-//	if err != nil {
-//		log.Fatal("Database connection failed")
-//	}
-//	requestBody := []byte("https://example.com54")
-//	wantBody := tools.HashURL(string(requestBody))
-//	_, err = http.NewRequest("POST", "/", strings.NewReader(string(requestBody)))
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	// Создаем записыватель ответа для захвата ответа.
-//	rr := httptest.NewRecorder()
-//
-//	// Вызываем хэндлер.
-//	handlers.HandlePost(conn)
-//
-//	assert.Equal(t, rr.Code, http.StatusCreated)
-//	assert.Equal(t, rr.Body, wantBody)
-//	// Проверяем тело ответа, если необходимо.
-//	// ...
-//
-//	// Добавляем другие проверки по мере необходимости.
-//}
-//
-//func TestJSONHandler(t *testing.T) {
-//	base, err := postgre.NewDataBase(context.Background(), *cfg.FlagDataBaseDSN)
-//	if err != nil {
-//		return
-//	}
-//
-//	requestBody := []byte(`{"url": "https://example.com4"}`)
-//	_, err = http.NewRequest("POST", "/", strings.NewReader(string(requestBody)))
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	// Создаем записыватель ответа для захвата ответа.
-//	rr := httptest.NewRecorder()
-//
-//	// Вызываем хэндлер.
-//	handlers.JSONHandler(base)
-//
-//	// Проверяем статус ответа.
-//	if rr.Code != http.StatusCreated {
-//		t.Errorf("Ожидался статус %d; получен %d", http.StatusCreated, rr.Code)
-//	}
-//	assert.Equal(t, rr.Code, http.StatusCreated)
-//	// Проверяем тело ответа, если необходимо.
-//	// ...
-//
-//	// Добавляем другие проверки по мере необходимости.
-//}
